fecd9ddc6a02768d570cda1e5f006c7b262d8d47
Siena Riparte <66598879+sienariparte@users.noreply.github.com>
2020-06-09 21:42:08 +0200

    WP2Static CREATE index.php

---
+++ index.php
@@ -0,0 +1,156 @@
+		<!DOCTYPE html>
+		<html lang="it-IT">
+		
+<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<title>Siena Riparte! &#8211; Cittadini al servizio della nostra città</title>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Siena Riparte! &raquo; Feed" href="http://localhost/index.php/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Siena Riparte! &raquo; Feed dei commenti" href="http://localhost/index.php/comments/feed/" />
+<link rel='stylesheet' id='wp-block-library-css'  href='http://localhost/wp-includes/css/dist/block-library/style.min.css?ver=5.4.1' type='text/css' media='all' />
+<link rel='stylesheet' id='parallax-frame-fonts-css'  href='https://fonts.googleapis.com/css?family=Open+Sans&#038;subset=latin%2Clatin-ext&#038;ver=1.0.0' type='text/css' media='all' />
+<link rel='stylesheet' id='parallax-frame-style-css'  href='http://localhost/wp-content/themes/parallax-frame/style.css?ver=1.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='http://localhost/wp-content/themes/parallax-frame/css/genericons/genericons.css?ver=3.4.1' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-sidr-css'  href='http://localhost/wp-content/themes/parallax-frame/css/jquery.sidr.light.min.css?ver=2.1.0' type='text/css' media='all' />
+<!--[if lte IE 9]>
+<link rel='stylesheet' id='parallax-frame-ie9-css'  href='http://localhost/wp-content/themes/parallax-frame/js/html5.min.js?ver=5.4.1' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='http://localhost/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='http://localhost/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/jquery.sidr.min.js?ver=2.2.1.1'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/custom-scripts.min.js'></script>
+<link rel='https://api.w.org/' href='http://localhost/index.php/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost/xmlrpc.php?rsd" />
+<meta name="generator" content="WordPress 5.4.1" />
+<link rel="icon" href="http://localhost/wp-content/uploads/2020/06/cropped-logo_240x240-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="http://localhost/wp-content/uploads/2020/06/cropped-logo_240x240-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon" href="http://localhost/wp-content/uploads/2020/06/cropped-logo_240x240-180x180.jpg" />
+<meta name="msapplication-TileImage" content="http://localhost/wp-content/uploads/2020/06/cropped-logo_240x240-270x270.jpg" />
+<!-- Siena Riparte! inline CSS Styles -->
+<style type="text/css" media="screen">
+.site-title a, .site-description { color: ##ffffff; }
+#supplementary { background-image: none; }
+</style>
+<!-- Siena Riparte! Header Image Background CSS Styles -->
+<style type='text/css' media='screen'>
+#header-featured-image { background-image: url("http://localhost/wp-content/uploads/2020/06/cropped-IMG_1605_v1.jpg"); }
+</style>
+</head>
+
+<body class="home blog two-columns content-left excerpt-image-left mobile-menu-one primary-search-enabled">
+
+
+		<div id="page" class="hfeed site">
+				<header id="masthead" class="fixed-header" role="banner">
+    		<div class="wrapper">
+		<div id="site-branding"><div id="site-header"><h1 class="site-title"><a href="http://localhost/">Siena Riparte!</a></h1><p class="site-description">Cittadini al servizio della nostra città</p></div><!-- #site-header --></div><!-- #site-branding-->	<nav id="site-navigation" class="nav-primary" role="navigation">
+    <h2 class="screen-reader-text">Primary Menu</h2>        <div class="primary-search-icon">
+            <div id="search-toggle" class="genericon">
+                <a class="screen-reader-text" href="#search-container">Search</a>
+            </div>
+
+            <div id="search-container" class="displaynone">
+                
+<form role="search" method="get" class="search-form" action="http://localhost/">
+	<label>
+		<span class="screen-reader-text"></span>
+		<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="">
+	</label>
+	<input type="submit" class="search-submit" value="Search">
+</form>
+            </div>
+        </div><!-- .primary-search-icon -->
+
+                <div id="mobile-header-right-menu" class="mobile-menu-anchor primary-menu">
+            <a href="#mobile-header-right-nav" id="header-right-menu" class="genericon genericon-menu">
+                <span class="mobile-menu-text">Menu</span>
+            </a>
+        </div><!-- #mobile-header-menu -->
+    </nav><!-- .nav-primary -->
+    			</div><!-- .wrapper -->
+		</header><!-- #masthead -->
+		
+			<div id="header-featured-image" class="parallax-effect">
+				<article class="hentry header-image">
+					<div class="entry-container">
+							<header class="entry-header">
+								<h2 class="entry-title">
+									Siena Riparte!
+								</h2>
+							</header>
+							<div class="entry-content"><p>
+								Comitato spontaneo di cittadini al servizio della loro città
+								<span class="readmore"><a href="mailto:sienariparte@gmail.com" class="button" target="_self">Contattaci!</a></span></p></div>
+					</div><!-- .entry-container -->
+				</article><!-- .hentry.header-image -->
+			</div><!-- #header-featured-image -->		<div id="content" class="site-content">
+			<div class="wrapper">
+	
+	<main id="main" class="site-main" role="main">
+
+		
+						
+				
+<article id="post-30" class="post-30 post type-post status-publish format-standard hentry category-comunicati-stampa excerpt-image-left">
+	<div class="archive-post-wrap">
+		
+		<div class="entry-container">
+			<header class="entry-header">
+				<h2 class="entry-title"><a href="http://localhost/index.php/2020/06/09/incontro-online-per-parlare-di-rilancio-del-turismo/" rel="bookmark">Incontro online per parlare di rilancio del turismo</a></h2>
+
+				
+					<p class="entry-meta"><span class="posted-on"><span class="screen-reader-text">Posted on</span><a href="http://localhost/index.php/2020/06/09/incontro-online-per-parlare-di-rilancio-del-turismo/" rel="bookmark"><time class="entry-date published" datetime="2020-06-09T19:10:00+00:00">9 Giugno 2020</time><time class="updated" datetime="2020-06-09T19:11:02+00:00">9 Giugno 2020</time></a></span><span class="comments-link"><a href="http://localhost/index.php/2020/06/09/incontro-online-per-parlare-di-rilancio-del-turismo/#respond">Leave a comment</a></span></p><!-- .entry-meta -->
+							</header><!-- .entry-header -->
+
+							<div class="entry-summary">
+					<p>&#8220;E’ possibile pensare a qualcosa per arginare l’erosione di risorse e permettere una continuità lavorativa tale da non compromettere del tutto la sopravvivenza stessa delle attività lavorative?&#8221; Il comitato spontaneo di Cittadini denominatosi “SienaRiparte!” preannuncia un evento online dove verranno <span class="readmore"><a class="more-link" href="http://localhost/index.php/2020/06/09/incontro-online-per-parlare-di-rilancio-del-turismo/">Read More &#8230;</a></span></p>
+				</div><!-- .entry-summary -->
+			
+			<footer class="entry-footer">
+				<p class="entry-meta"></p><!-- .entry-meta -->			</footer><!-- .entry-footer -->
+		</div><!-- .entry-container -->
+	</div><!-- .archive-post-wrap -->
+</article><!-- #post -->
+
+			
+			
+			</main><!-- #main -->
+
+
+	<aside class="sidebar sidebar-primary widget-area" role="complementary">
+				<section id="recent-posts-2" class="widget widget_recent_entries"><div class="widget-wrap">		<h2 class="widget-title">Articoli recenti</h2>		<ul>
+											<li>
+					<a href="http://localhost/index.php/2020/06/09/incontro-online-per-parlare-di-rilancio-del-turismo/">Incontro online per parlare di rilancio del turismo</a>
+									</li>
+					</ul>
+		</div><!-- .widget-wrap --></section><!-- #widget-default-search --><section id="categories-2" class="widget widget_categories"><div class="widget-wrap"><h2 class="widget-title">Categorie</h2>		<ul>
+				<li class="cat-item cat-item-3"><a href="http://localhost/index.php/category/comunicati-stampa/">Comunicati stampa</a>
+</li>
+		</ul>
+			</div><!-- .widget-wrap --></section><!-- #widget-default-search --><section id="parallax_frame_social_icons-3" class="widget widget_parallax_frame_social_icons"><div class="widget-wrap"><h2 class="widget-title">Siamo anche su...</h2><a class="genericon_parent genericon genericon-facebook-alt" target="_blank" title="Facebook" href="https://www.facebook.com/sienariparte"><span class="screen-reader-text">Facebook</span> </a><a class="genericon_parent genericon genericon-mail" target="_blank" title="Email" href="mailto:s&#105;e&#110;a&#114;&#105;&#112;&#97;rt&#101;&#64;gm&#97;&#105;&#108;&#46;com"><span class="screen-reader-text">Email</span> </a></div><!-- .widget-wrap --></section><!-- #widget-default-search -->	</aside><!-- .sidebar sidebar-primary widget-area -->
+
+			</div><!-- .wrapper -->
+	    </div><!-- #content -->
+		
+	<footer id="colophon" class="site-footer" role="contentinfo">
+    
+    	<div id="site-generator" class="two">
+    		<div class="wrapper">
+    			<div id="footer-left-content" class="copyright">Copyright &copy; 2020 <a href="http://localhost/">Siena Riparte!</a>. All Rights Reserved. </div>
+
+    			<div id="footer-right-content" class="powered">Parallax Frame&nbsp;by&nbsp;<a target="_blank" href="https://catchthemes.com/">Catch Themes</a></div>
+			</div><!-- .wrapper -->
+		</div><!-- #site-generator -->	</footer><!-- #colophon -->
+			</div><!-- #page -->
+		
+<a href="#masthead" id="scrollup" class="genericon"><span class="screen-reader-text">Scroll Up</span></a><nav id="mobile-header-right-nav" class="mobile-menu" role="navigation"></nav><!-- #mobile-header-right-nav -->
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/navigation.min.js?ver=1.3.2'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/skip-link-focus-fix.min.js?ver=1.3.2'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/fitvids.min.js?ver=1.1'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/jquery.cycle/jquery.cycle2.min.js?ver=2.1.5'></script>
+<script type='text/javascript' src='http://localhost/wp-content/themes/parallax-frame/js/scrollup.min.js?ver=1.3.2'></script>
+
+</body>
+</html>
